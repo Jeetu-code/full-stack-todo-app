eddac1f2b6d8e799f2e03ba7d97f055190059602
jeetu <jk1871881@gmail.com>
2026-01-02 14:23:03 +0530

    cors origin error resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,10 @@ const route=require("./routes/user.route.js");
 const globalerr=require("./middlewares/globalerr.js");
 app.use(express.json());
 app.use(cors({
-	origin:"https://full-stack-todo-app-neon.vercel.app/",
+	origin:"https://full-stack-todo-app-neon.vercel.app",
 	methods:["POST","GET","PUT","DELETE"],
 	allowedHeaders:["Content-Type","Authorization"],
-	Credentials:true
+	credentials:true
 
 }));
 app.use("/user",route);
